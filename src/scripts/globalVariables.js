11a58db8dc8a2a755779d9bcc65917c944de17aa
Mondal10 <amitmondal111.am@gmail.com>
2020-01-18 20:33:17 +0530

    File structuring and adding css and scripts

---
+++ src/scripts/globalVariables.js
@@ -0,0 +1,14 @@
+export const data = {
+  greeting: "UNIDENTIFIED ORGANIC LIFEFORM DETECTED",
+  message: "RUNNING GITHUB RESUME PROTOCAL (GRP) FOR @MONDAL10",
+  name: "NAME: Amit Mondal",
+  alias: "KNOWN ALIAS: Mondal10",
+  occupation: "OCCUPATION: FULLSTACK DEVELOPER",
+  frontEnd: "FRONT END DEVELOPMENT CORE: HTML5 | CSS3 | JAVASCRIPT",
+  backEnd: "BACK END DEVELOPEMNT: NodeJS | Python | PHP | MySQL",
+  JSFrameworks: "JAVASCRIPT FRAMEWORKS: ReactJS | VueJS",
+  JSLibs: "JAVASCRIPT LIBRARIES: jQuery",
+  CSSFrameworks: "CSS FRAMEWORKS: Bootstrap | Bulma | Buefy | Materialize",
+  CSSPre = "CSS PREPROCESSORS: SASS",
+  frontEndAnimation = "FRONT END ANIMATION: Canvas | HTML5"
+};
