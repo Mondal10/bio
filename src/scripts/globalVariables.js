7ddf4adff749cdede1655ea3a92968e7a026a322
Mondal10 <amitmondal111.am@gmail.com>
2020-01-20 21:42:31 +0530

    typo error fix

--- src/scripts/globalVariables.js
+++ src/scripts/globalVariables.js
@@ -1,14 +1,14 @@
 export const data = {
-  greeting: "UNIDENTIFIED ORGANIC LIFEFORM DETECTED",
-  message: "RUNNING GITHUB RESUME PROTOCAL (GRP) FOR @MONDAL10",
+  greeting: "UNIDENTIFIED ORGANIC LIFE-FORM DETECTED",
+  message: "RUNNING GITHUB RESUME PROTOCOL (GRP) FOR @MONDAL10",
   name: "NAME: Amit Mondal",
   alias: "KNOWN ALIAS: Mondal10",
   occupation: "OCCUPATION: Fullstack Developer",
   frontEnd: "FRONT END DEVELOPMENT CORE: HTML5 | CSS3 | JAVASCRIPT",
-  backEnd: "BACK END DEVELOPEMNT: NodeJS | Python | PHP | MySQL",
+  backEnd: "BACK END DEVELOPMENT: NodeJS | Python | PHP | MySQL",
   JSFrameworks: "› JAVASCRIPT FRAMEWORKS: ReactJS | VueJS",
   JSLibs: "› JAVASCRIPT LIBRARIES: jQuery",
   CSSFrameworks: "› CSS FRAMEWORKS: Bootstrap | Bulma | Buefy | Materialize",
-  CSSPre: "› CSS PREPROCESSORS: SASS",
+  CSSPre: "› CSS PRE-PROCESSORS: SASS",
   frontEndAnimation: "› FRONT END ANIMATION: Canvas | HTML5"
 };
